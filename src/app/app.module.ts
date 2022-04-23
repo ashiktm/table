7337add6e2eae93484ca62cfebde2b69c454d7da
ashik <ashiktm.ind@gmail.com>
2022-04-23 12:25:10 +0530

    task2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,44 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EditRowComponent } from './edit-row/edit-row.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-
+import { RowEditComponent } from './row-edit/row-edit.component';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProductService } from './productservice';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ToastModule } from 'primeng/toast';
+import { CalendarModule } from 'primeng/calendar';
+import { SliderModule } from 'primeng/slider';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+// import {DropdownModule} from 'primeng/dropdown';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { InputTextModule } from 'primeng/inputtext';
 @NgModule({
-  declarations: [AppComponent, EditRowComponent],
+  declarations: [AppComponent, EditRowComponent, RowEditComponent],
   imports: [
     BrowserModule,
-    TableModule,
-    ReactiveFormsModule,
-    FormsModule,
     BrowserAnimationsModule,
+    TableModule,
+    CalendarModule,
+    SliderModule,
+    DialogModule,
+    MultiSelectModule,
+    ContextMenuModule,
+    DropdownModule,
+    ButtonModule,
+    ToastModule,
+    InputTextModule,
+    ProgressBarModule,
+    HttpClientModule,
+    FormsModule,
     MatDialogModule,
     MatInputModule,
+    ReactiveFormsModule,
     MatButtonModule,
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/row-edit/row-edit.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../productservice';
+import { SelectItem } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { FormGroup } from '@angular/forms';
+@Component({
+  selector: 'app-row-edit',
+  templateUrl: './row-edit.component.html',
+  styleUrls: ['./row-edit.component.scss'],
+  providers: [MessageService],
+  styles: [
+    `
+      :host ::ng-deep .p-cell-editing {
+        padding-top: 0 !important;
+        padding-bottom: 0 !important;
+      }
+    `,
+  ],
+})
+export class RowEditComponent implements OnInit {
+  products1: Product[] = [];
+
+  products2: Product[] = [];
+  editForm: FormGroup = new FormGroup({});
+
+  statuses: SelectItem[] = [];
+
+  clonedProducts: { [s: string]: Product } = {};
+
+  constructor(
+    private productService: ProductService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit() {
+    this.productService
+      .getProductsSmall()
+      .then((data) => (this.products1 = data));
+    this.productService
+      .getProductsSmall()
+      .then((data) => (this.products2 = data));
+
+    this.statuses = [
+      { label: 'In Stock', value: 'INSTOCK' },
+      { label: 'Low Stock', value: 'LOWSTOCK' },
+      { label: 'Out of Stock', value: 'OUTOFSTOCK' },
+    ];
+  }
+
+  onRowEditInit(product: Product) {
+    this.clonedProducts[product.id!] = { ...product };
+  }
+
+  onRowEditSave(product: Product, index: number) {
+    if (
+      product.price! > 0 &&
+      product.name! != '' &&
+      product.name?.length! >= 4 &&
+      product.id! != ''
+    ) {
+      delete this.clonedProducts[product.id!];
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Product is updated',
+      });
+    } else {
+      // this.products2[index] = this.clonedProducts[product.id!];
+
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Invalid Price',
+      });
+    }
+  }
+
+  onRowEditCancel(product: Product, index: number) {
+    this.products2[index] = this.clonedProducts[product.id!];
+    delete this.clonedProducts[product.id!];
+  }
+}
